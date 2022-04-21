dc586dce4bfe16fa8e8162cb1a18ed79865d0ea0
rishabhmthakur2 <rishabhmthakur@outlook.com>
2022-04-21 14:48:56 -0700

    Correcting URL Splitting issues

--- utils/metadata.js
+++ utils/metadata.js
@@ -2,22 +2,25 @@ const axios = require("axios");
 
 const getIPFSURL = (uri) => {
   const urlParts = uri.split("://");
-  return urlParts[0], urlParts[1];
+  return urlParts;
 };
 const fetchMetadata = async (uri) => {
-  let prefix,
-    ipfsURL = getIPFSURL(uri);
+  let urlParts = getIPFSURL(uri);
+  const prefix = urlParts[0];
+  const ipfsURL = urlParts[1];
+  console.log({ prefix, ipfsURL });
   let metadata = "";
   if (prefix === "ipfs") {
     metadata = await axios.get(`https://ipfs.io/ipfs/${ipfsURL}`);
   } else {
     metadata = await axios.get(uri);
   }
+  console.log({ metadata });
   return {
     name: metadata.data.name,
     image:
       prefix === "ipfs"
-        ? `https://ipfs.io/ipfs/${getIPFSURL(metadata.data.image)}`
+        ? `https://ipfs.io/ipfs/${getIPFSURL(metadata.data.image)[1]}`
         : metadata.data.image,
   };
 };
