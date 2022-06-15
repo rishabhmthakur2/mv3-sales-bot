148013933356e322eebf2468015ddb3dd2941eca
rishabhmthakur2 <rishabhmthakur05@gmail.com>
2022-06-15 12:02:50 -0700

    Updating event listener to support OS port to SeaPort

--- getEvents.js
+++ getEvents.js
@@ -59,7 +59,7 @@ let getEvents = async () => {
   // Listening for "Transfer" event
   myContract.events
     .Transfer({
-      fromBlock: 14965684 //await web3.eth.getBlockNumber(), // Gets the latest block everytime when the app is started. Will start listening to events that occur after this Block.
+      fromBlock: 14965684, //await web3.eth.getBlockNumber(), // Gets the latest block everytime when the app is started. Will start listening to events that occur after this Block.
     })
     .on("connected", function (subscriptionId) {
       console.log({ subscriptionId });
@@ -70,89 +70,118 @@ let getEvents = async () => {
       const txReceipt = await web3.eth.getTransactionReceipt(
         res.transactionHash
       );
+      // Updated Types Array for SeaPort OS Port
       const typesArray = [
-        { type: "bytes32", name: "buyHash" },
-        { type: "bytes32", name: "sellHash" },
-        { type: "uint256", name: "price" },
+        { type: "bytes32", name: "hash" },
+        { type: "address", name: "buyerAddress" },
+        { type: "uint256", name: "f1" },
+        { type: "uint256", name: "f2" },
+        { type: "uint256", name: "f3" },
+        { type: "uint256", name: "f4" },
+        { type: "address", name: "contractAddress" },
+        { type: "uint256", name: "tokenID" },
+        { type: "uint256", name: "f5" },
+        { type: "uint256", name: "f6" },
+        { type: "uint256", name: "f7" },
+        { type: "uint256", name: "f8" },
+        { type: "uint256", name: "f9" },
+        { type: "uint256", name: "payoutOne" },
+        { type: "address", name: "sellerAddress" },
+        { type: "uint256", name: "f10" },
+        { type: "uint256", name: "f11" },
+        { type: "uint256", name: "f12" },
+        { type: "uint256", name: "payoutTwo" },
+        { type: "address", name: "openseaWallet" },
+        { type: "uint256", name: "f13" },
+        { type: "uint256", name: "f14" },
+        { type: "uint256", name: "f15" },
+        { type: "uint256", name: "payoutThree" },
+        { type: "address", name: "commissionWallet" },
       ];
       txReceipt?.logs.forEach(async (log) => {
         if (
+          // Topic specifically for OS as of now - Needs to be changed
           log.topics[0] ===
-            "0xc4109843e0b7d514e4c093114b863f8e7d8d9a458c372cd51bfe526b588006c9" &&
-          (log.topics[1]?.toLowerCase() ==
-            web3.utils.padLeft(res.returnValues.from, 64).toLowerCase() ||
-            log.topics[2]?.toLowerCase() ==
-              web3.utils.padLeft(res.returnValues.from, 64).toLowerCase())
+          "0x9d9af8e38d66c62e2c12f0225249fd9d721c54b83f48d9352c97c6cacdcb6f31"
         ) {
-          const decodedParameters = web3.eth.abi.decodeParameters(
+          const { ...decodedParameters } = web3.eth.abi.decodeParameters(
             typesArray,
             log.data
           );
-          const v = `${parseInt(decodedParameters.price)}`;
-          const value = new BN(web3.utils.fromWei(v));
-          if (value.gt(0)) {
-            // Getting the Block of Transaction
-            const block = await web3.eth.getBlock(res.blockNumber);
-            // Creating a JSON that will hold all the data
-            let message = {
-              value: value.toFixed(), // Value of transaction
-              to: res.returnValues.to, // NFT Transferred To (Buyer)
-              from: res.returnValues.from, // NFT Transferred From (Seller)
-              timestamp: block.timestamp, // Timestamp of the transaction (in UNIX)
-              tokenId: res.returnValues.tokenId, // Token ID of the NFT transferred/Sold
-              txHash: res.transactionHash, // Transaction Hash
-            };
+          if (
+            decodedParameters.sellerAddress == res.returnValues.from &&
+            decodedParameters.buyerAddress == res.returnValues.to
+          ) {
+            const totalPrice =
+              parseInt(decodedParameters.payoutOne) +
+              parseInt(decodedParameters.payoutTwo) +
+              parseInt(decodedParameters.payoutThree);
+            const v = `${parseInt(totalPrice)}`;
+            const value = new BN(web3.utils.fromWei(v));
+            if (value.gt(0)) {
+              // Getting the Block of Transaction
+              const block = await web3.eth.getBlock(res.blockNumber);
+              // Creating a JSON that will hold all the data
+              let message = {
+                value: value.toFixed(), // Value of transaction
+                to: res.returnValues.to, // NFT Transferred To (Buyer)
+                from: res.returnValues.from, // NFT Transferred From (Seller)
+                timestamp: block.timestamp, // Timestamp of the transaction (in UNIX)
+                tokenId: res.returnValues.tokenId, // Token ID of the NFT transferred/Sold
+                txHash: res.transactionHash, // Transaction Hash
+              };
 
-            // Checking if the transfer was a mint or a sale
-            if (
-              res.returnValues.from !=
-                "0x0000000000000000000000000000000000000000" &&
-              parseFloat(value.toFixed()) > 0
-            ) {
-              // Consoling the Message Block generate above
-              console.log({ message });
-              try {
-                const isDuplicate = await checkDataDuplicate(
-                  mongoClient,
-                  message.from,
-                  message.to,
-                  message.timestamp,
-                  message.txHash
-                );
-                if (!isDuplicate) {
-                  const newRecord = await insertRecordToMongo(
+              // Checking if the transfer was a mint or a sale
+              if (
+                res.returnValues.from !=
+                  "0x0000000000000000000000000000000000000000" &&
+                parseFloat(value.toFixed()) > 0
+              ) {
+                // Consoling the Message Block generate above
+                console.log({ message });
+                try {
+                  const isDuplicate = await checkDataDuplicate(
                     mongoClient,
-                    message
+                    message.from,
+                    message.to,
+                    message.timestamp,
+                    message.txHash
                   );
-                  console.log({
-                    message: `Adding transaction to database with id: ${newRecord.insertedId.toString()}`,
-                  });
-                  const uri = await myContract.methods
-                    .tokenURI(message.tokenId)
-                    .call();
-                  const metadata = await fetchMetadata(uri);
-                  message = {
-                    ...message,
-                    name: metadata.name,
-                    image: metadata.image,
-                  };
-                  try {
-                    const tweetData = await sendTweet(message);
-                    console.log({ tweetData });
-                  } catch (e) {
-                    console.error(e);
-                  }
-                  try {
-                    const discordMessageData = await sendDiscordMessage(
+                  if (!isDuplicate) {
+                    const newRecord = await insertRecordToMongo(
+                      mongoClient,
                       message
                     );
-                    console.log({ discordMessageData });
-                  } catch (e) {
-                    console.error(e);
+                    console.log({
+                      message: `Adding transaction to database with id: ${newRecord.insertedId.toString()}`,
+                    });
+                    const uri = await myContract.methods
+                      .tokenURI(message.tokenId)
+                      .call();
+                    const metadata = await fetchMetadata(uri);
+                    message = {
+                      ...message,
+                      name: metadata.name,
+                      image: metadata.image,
+                    };
+                    try {
+                      const tweetData = await sendTweet(message);
+                      console.log({ tweetData });
+                    } catch (e) {
+                      console.error(e);
+                    }
+                    try {
+                      const discordMessageData = await sendDiscordMessage(
+                        message
+                      );
+                      console.log({ discordMessageData });
+                    } catch (e) {
+                      console.error(e);
+                    }
                   }
+                } catch (e) {
+                  console.error(e);
                 }
-              } catch (e) {
-                console.error(e);
               }
             }
           }
